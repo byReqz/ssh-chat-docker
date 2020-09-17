39de1ab23e76412fb37ee25fda75177933b8feb5
Nils <32552517+byReqz@users.noreply.github.com>
2020-09-17 11:12:52 +0000

    adding launch parameter for admin keys

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -f ssh_key ]; then
-	/ssh-chat -i /ssh_key $@
+	/ssh-chat -i /ssh_key --admin=/admin_keys $@
 else
 	echo "SSH Key not found at /ssh_key. Generating..."
 	ssh-keygen -t rsa -b 4096 -f /ssh_key -q -N ''
-	/ssh-chat -i /ssh_key $@
+	/ssh-chat -i /ssh_key --admin /admin_keys $@
 fi
