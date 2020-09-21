02e897acdf4a28035db8e5526a386a2d1ad8de13
Nils <32552517+byReqz@users.noreply.github.com>
2020-09-21 11:08:10 +0000

    adding motd support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -f ssh_key ]; then
-	/ssh-chat -i /ssh_key --admin=/admin_keys $@
+	/ssh-chat -i /ssh_key --admin=/admin_keys --motd=/motd $@
 else
 	echo "SSH Key not found at /ssh_key. Generating..."
 	ssh-keygen -t rsa -b 4096 -f /ssh_key -q -N ''
-	/ssh-chat -i /ssh_key --admin /admin_keys $@
+	/ssh-chat -i /ssh_key --admin /admin_keys --motd=/motd $@
 fi
